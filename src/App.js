89bf246b5f8c1003d4b8472cd9161d7d91101b94
mkeloo <mkeloo@ufl.edu>
2022-10-23 05:58:04 -0400

    local storage check

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
 import Header from './components/Header';
+import AddItem from './components/AddItem';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import { useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: 'One half pound bag of Almonds Unsalted',
-    },
-    {
-      id: 2,
-      checked: false,
-      item: 'Item 2',
-    },
-    {
-      id: 3,
-      checked: false,
-      item: 'Item 3',
-    },
-  ]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('Shopping List'))
+  );
+
+  const [newItem, setNewItem] = useState('');
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('Shopping List', JSON.stringify(newItems));
+  };
+
+  const addItem = (item) => {
+    // Create a new item object id
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    // Create a new item object with unchecked and item
+    const myNewItem = { id, checked: false, item };
+    // Add the new item to the items array using the spread operator
+    const listItems = [...items, myNewItem];
+    // Set the items array to the new listItems array
+    setAndSaveItems(listItems);
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-
-    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    // e.preventDefault(); // Prevents page refresh or reload
+    e.preventDefault();
+    if (!newItem) return;
+    // add item function and then resetting the input field using setNewItem to an empty string
+    addItem(newItem);
+    setNewItem('');
   };
 
   // Props and Props Drilling
@@ -42,6 +53,11 @@ function App() {
   return (
     <div className="App">
       <Header title="Groceries List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         handleCheck={handleCheck}
